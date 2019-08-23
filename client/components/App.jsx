d06d06bba0256c930fe9f4560018c973a4e2debb
Bryan Liang <bryanliang1995@gmail.com>
2019-08-23 14:47:49 -0700

    Now only renders three places at a time

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,16 +6,43 @@ import PlaceList from './PlaceList';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.places = [];
     this.state = {
       places: [],
+      index: 0,
     };
+    this.nextPlace = this.nextPlace.bind(this);
+    this.prevPlace = this.prevPlace.bind(this);
   }
 
   componentDidMount() {
     axios('/api/nearbyPlaces/1').then((response) => {
+      this.places = response.data;
       this.setState({
-        places: response.data,
+        places: response.data.slice(0, 3),
       });
+    });
+  }
+
+  nextPlace() {
+    this.setState((state) => {
+      const index = state.index + 1;
+      const places = this.places.slice(index, index + 3);
+      return {
+        index,
+        places,
+      };
+    });
+  }
+
+  prevPlace() {
+    this.setState((state) => {
+      const index = state.index - 1;
+      const places = this.places.slice(index, index + 3);
+      return {
+        index,
+        places,
+      };
     });
   }
 
