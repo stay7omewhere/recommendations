a54d022edd10638779f4123ca9eeb0ca8b329341
Bryan Liang <bryanliang1995@gmail.com>
2019-08-23 14:44:31 -0700

    Add more tests

--- __tests__/place.test.js
+++ __tests__/place.test.js
@@ -20,26 +20,51 @@ describe('Place Component', () => {
     url: 'test.jpeg',
   };
 
+  const verifiedTestPlace = {
+    _id: '1231sd5a1sd5a6sd1',
+    averageReview: 4.931936565117398,
+    city: 'North Carolanne',
+    id: 1,
+    plusVerified: true,
+    price: 210,
+    propertyType: 'ut aut ut',
+    savedList: ['incidunt', 'quibusdam', 'consequatur'],
+    title: 'Tenetur numquam sed ut non ex voluptas ut qui.',
+    totalReviews: 149,
+    url: 'test.jpeg',
+  };
+  const wrapperVerified = shallow(<Place place={verifiedTestPlace} />);
+  const wrapper = shallow(<Place place={testPlace} />);
+
   it('should render a picture', () => {
-    const wrapper = shallow(<Place place={testPlace} />);
-    expect(wrapper.containsMatchingElement(<img src="test.jpeg" />)).toBe(true);
+    expect(wrapper.find('Image').prop('src')).toBe('test.jpeg');
   });
 
   it('should render a title', () => {
-    const wrapper = shallow(<Place place={testPlace} />);
-    expect(wrapper.containsMatchingElement(
-      <div>Tenetur numquam sed ut non ex voluptas ut qui.</div>,
-    )).toBe(true);
+    expect(wrapper.find('Title').text()).toBe('Tenetur numquam sed ut non ex voluptas ut qui.');
   });
 
   it('should render the property type and city', () => {
-    const wrapper = shallow(<Place place={testPlace} />);
-    expect(wrapper.containsMatchingElement(
-      <div>
-        <span>ut aut ut</span>
-        <span> · </span>
-        North Carolanne
-      </div>,
-    )).toBe(true);
+    expect(wrapper.find('Property').text()).toBe('ut aut ut · North Carolanne');
+  });
+
+  it('should render the price', () => {
+    expect(wrapper.find('Price').text()).toBe('$210/night');
+  });
+
+  it('should render the total reviews', () => {
+    expect(wrapper.find('Review').text().includes('149'));
+  });
+
+  it('should render stars', () => {
+    expect(wrapper.find('Stars').text()).toBe('★★★★★★★★★★');
+  });
+
+  it('should render a plus tag for plus verified places', () => {
+    expect(wrapperVerified.find('PlusVerified').exists());
+  });
+
+  it('should not render a plus tag for non verified places', () => {
+    expect(wrapper.find('PlusVerified').exists()).toBeFalsy();
   });
 });
