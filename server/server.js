c9cadcca1642a86f245728746fe246e8955703af
Bryan Liang <bryanliang1995@gmail.com>
2019-09-08 15:58:40 -0700

    Add compression for better page load speeds

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const model = require('../database/index');
 // const path = require('path');
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(require('morgan')('dev'));
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
